b7fc30ed540a2c3695622c0daca0c1167c42be81
Jacob Lambert <jacob.lambert@robotics.utias.utoronto.ca>
2015-12-08 13:10:22 -0500

    added SLICO algorithm and a vision directory with the image segmentation script

---
+++ vision/SLICO.m
@@ -0,0 +1,20 @@
+function [labels,numlabels] = SLICO(img,showImgs)
+% GetSP computes superpixels using SLICO for a grayscale or RGB 'img'
+% showImg = 1 if you want it to show figures
+
+[labels,numlabels] = slicomex(img,1242);
+
+if showImgs == 1
+% show image before superpixels
+    figure(1)
+    imagesc(img)
+% show image superpixel labels
+    figure(2)
+    imagesc(labels)
+% show the superpixel borders
+    border_img = superpixelBorder(img,labels);
+    figure(3)
+    imagesc(border_img)
+end
+
+end
---
+++ vision/superpixelBorder.m
@@ -0,0 +1,32 @@
+function [border_img] = superpixelBorder(img,labels)
+% Shows the boundaries of the superpixels computed by SLICO (denoted by labels) 
+% on the img.
+
+N = size(labels,1);
+M = size(labels,2);
+border_img = img;
+
+% Loop left-right
+for i=1:N
+    for j=1:(M-1)
+        if labels(i,j)~=labels(i,j+1)
+            border_img(i,j,:)=0;
+            %border_img(i,j+1,:)=0;
+        end
+    end
+end
+
+% Loop up-down
+
+for j=1:M
+    for i=1:(N-1)
+        if labels(i,j)~=labels(i+1,j)
+            border_img(i,j,:)=0;
+            %border_img(i+1,j,:)=0;
+        end
+    end
+end
+        
+
+end
+
