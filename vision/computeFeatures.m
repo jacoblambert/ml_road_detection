286355fde688e058d3a9168b951bee53e0d1ac1a
Jacob Lambert <jacob.lambert@robotics.utias.utoronto.ca>
2015-12-18 14:44:40 -0500

    created 3 training sets in data NxD

--- vision/computeFeatures.m
+++ vision/computeFeatures.m
@@ -1,4 +1,4 @@
-function [ feat ] = computeFeatures(img, labels)
+function [ feat ] = computeFeatures(img, superpixelLabels)
 
 % This script computes a series of features for the superpixels on an image
 % feat is a structure with fields .rhist )red color histogram
@@ -7,22 +7,14 @@ function [ feat ] = computeFeatures(img, labels)
 %                                 .centroid (x,y), 
 %                                 .gradient colorhist (?)
 
-% Parameters
-nBins = 256; % Number of bins for the color histograms
-
-% Compute superpixel labels
-[superpixelLabels, ~] = SLICO(img,1);
-
-% Compute gradients
+%% Compute gradients
 Gmag = zeros(size(img));
-Gdir = Gmag;
 for channel=1:3
-   [Gmag(:,:,channel),Gdir(:,:,channel)] = imgradient(img(:,:,channel),'CentralDifference');
+   [Gmag(:,:,channel),~] = imgradient(img(:,:,channel),'CentralDifference');
    Gmag(:,:,channel) = Gmag(:,:,channel)/max(max(Gmag(:,:,channel)));
-   Gdir(:,:,channel) = Gdir(:,:,channel)/max(max(Gdir(:,:,channel)));
 end
 
-% Compute features on superpixels
+%% Compute features on superpixels
 for i=0:max(max(superpixelLabels))
     % Extract superpixel and pixels labels
         [I,J] = find(superpixelLabels==i); % Indices row I and column J
@@ -36,23 +28,19 @@ for i=0:max(max(superpixelLabels))
     % Compute RGBHist of pixels
         px = double(px);
         feat.rmean(i+1,:) = mean(px(:,:,1)); feat.rvar(i+1,:) = var(px(:,:,1));
-        feat.bmean(i+1,:) = mean(px(:,:,2)); feat.bvar(i+1,:) = var(px(:,:,2));
-        feat.gmean(i+1,:) = mean(px(:,:,3)); feat.gvar(i+1,:) = var(px(:,:,3));
+        feat.gmean(i+1,:) = mean(px(:,:,2)); feat.gvar(i+1,:) = var(px(:,:,2));
+        feat.bmean(i+1,:) = mean(px(:,:,3)); feat.bvar(i+1,:) = var(px(:,:,3));
     % Compute something from the Gmag/Gdir images
         Gmag = double(Gmag);
         feat.grmean(i+1,:) = mean(Gpx(:,:,1)); feat.grvar(i+1,:) = var(Gpx(:,:,1));
-        feat.gbmean(i+1,:) = mean(Gpx(:,:,2)); feat.gbvar(i+1,:) = var(Gpx(:,:,2));
-        feat.ggmean(i+1,:) = mean(Gpx(:,:,3)); feat.ggvar(i+1,:) = var(Gpx(:,:,3));
+        feat.ggmean(i+1,:) = mean(Gpx(:,:,2)); feat.ggvar(i+1,:) = var(Gpx(:,:,2));
+        feat.gbmean(i+1,:) = mean(Gpx(:,:,3)); feat.gbvar(i+1,:) = var(Gpx(:,:,3));
 end
     
-% Plot image with superpixels
-border_img = superpixelBorder(img,superpixelLabels);
-figure(1)
-imagesc(border_img)
-hold on
-scatter(feat.centroid(:,2),feat.centroid(:,1),'g')
-
-if nargin>1
-    feat.labels = labels;
-end
+%% Plot image with superpixels
+% border_img = superpixelBorder(img,superpixelLabels);
+% figure(1)
+% imagesc(border_img)
+% hold on
+% scatter(feat.centroid(:,2),feat.centroid(:,1),'g')
 
---
+++ vision/computeLabels.m
@@ -0,0 +1,35 @@
+function [ labels ] = computeLabels(lbl,superpixelLabels)
+% Given a binary image, compute the labels of the superpixels 
+
+%% Reshape lbl if it is smaller than superpixelLabels
+N=size(superpixelLabels,1); M=size(superpixelLabels,2);
+n=size(lbl,1); m=size(lbl,2);
+if (n<N || m<M)
+    fprintf('superpixel img is %d-by-%d and ground truth img is %d-by-%d, reshaping!\n',N,M,n,m);
+    temp = zeros(max(n,N),max(m,M)); temp(max(n,N)-n+1:end,max(m,M)-m+1:end)=lbl; lbl=temp;
+end
+    
+%% Compute label
+for i=0:max(max(superpixelLabels))
+    % Extract superpixel and pixels labels
+        [I,J] = find(superpixelLabels==i); % Indices row I and column J
+        votes = diag(lbl(I,J));
+        labels(i+1)=round(sum(votes)/length(votes));
+end
+
+%% Visualize
+% superImage = zeros(n,m);
+% for k=0:max(max(superpixelLabels))
+%     [I,J] = find(superpixelLabels==k);
+%     if labels(k+1)==1
+%         for i=1:length(I)
+%             superImage(I(i),J(i))=labels(k+1);
+%         end
+%     end
+% end
+% figure(1)
+% imagesc(superImage);
+% figure(2)
+% imagesc(lbl);
+%         
+        
